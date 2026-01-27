4b839e83f381d4e5d77927a566e3350f3f05fc72
abramovich-maks <maks.abramovich.1995@mail.ru>
2026-01-27 13:58:26 +0100

    Add solution for "2. Shuffle the Array" problem

---
+++ src/main/java/leetcodequest/ShuffleTheArray_Q2.java
@@ -0,0 +1,54 @@
+package leetcodequest;
+
+import java.util.Arrays;
+
+/*
+* Q2. Shuffle the Array
+Easy
+*
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+*
+Example 1:
+Input: nums = [2,5,1,3,4,7], n = 3
+Output: [2,3,5,4,1,7]
+Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+*
+Example 2:
+Input: nums = [1,2,3,4,4,3,2,1], n = 4
+Output: [1,4,2,3,3,2,4,1]
+*
+Example 3:
+Input: nums = [1,1,2,2], n = 2
+Output: [1,2,1,2]
+*
+Constraints:
+1 <= n <= 500
+nums.length == 2n
+1 <= nums[i] <= 10^3
+* */
+class ShuffleTheArray_Q2 {
+    public static void main(String[] args) {
+
+        int[] arr1 = {2, 5, 1, 3, 4, 7};
+        int[] arr2 = {1, 2, 3, 4, 4, 3, 2, 1};
+        int[] arr3 = {1, 1, 2, 2};
+
+        System.out.println(Arrays.toString(shuffle(arr1, 3)));
+        System.out.println(Arrays.toString(shuffle(arr2, 4)));
+        System.out.println(Arrays.toString(shuffle(arr3, 2)));
+    }
+
+    public static int[] shuffle(int[] nums, int n) {
+        int[] result = new int[nums.length];
+        int x = 0;
+        int y = n;
+        int num = 0;
+
+        while (num < nums.length) {
+            result[num++] = nums[x++];
+            result[num++] = nums[y++];
+        }
+        return result;
+    }
+}
